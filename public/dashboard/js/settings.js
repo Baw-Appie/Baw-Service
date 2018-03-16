7ad8d88d136be28d30562051ead7e0febe2529eb
Baw Appie <pp121324@gmail.com>
2018-03-16 18:14:40 +0900

    사이드바 & 오프캔버스 적당히 조절하여 사용할 수 있도록 수정 및 Baw Service 페이지 로드 가속이 모바일에서 작동하지 않는 문제 수정

--- public/dashboard/js/settings.js
+++ public/dashboard/js/settings.js
@@ -1,10 +1,9 @@
 window.onload = function () {
   //var url = location.href
   //$.pjax({url: url, container: '#pjax'})
-  toastr.info('Baw Service Dashboard Beta 4 초기 설정중..', '환영합니다!');
+  //toastr.info('Baw Service Dashboard Beta 4 초기 설정중..', '환영합니다!');
 }
-
-$(document).pjax('a', '#pjax')
+$( document ).pjax( 'a', '#contents' );
 $(document).on('pjax:start', function() { NProgress.start(); });
 $(document).on('pjax:end',   function() { NProgress.done();  });
 
