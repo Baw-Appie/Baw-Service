d25f409d6f5c72b05ad57f97e822f2895e397be6
Baw_Appie <pp121324@gmail.com>
2018-03-18 11:47:32 +0900

    포트는 언제든지 바뀔수도 있는 법

--- libs/hostname.js
+++ libs/hostname.js
@@ -1,7 +1,23 @@
+var config = require('../config/server_settings');
 module.exports = function() {
     return function hostname( req, res, next ) {
-        req.hostname = req.hostname;
-        res.locals.hostname = req.hostname;
+        if (req.protocol == "http") {
+          if (config.http_port != 80) {
+            req.hostname = req.hostname+':'+config.http_port;
+            res.locals.hostname = req.hostname+':'+config.http_port;
+          } else {
+            req.hostname = req.hostname;
+            res.locals.hostname = req.hostname;
+          }
+        } else {
+          if (config.http_port != 443) {
+            req.hostname = req.hostname+':'+config.https_port;
+            res.locals.hostname = req.hostname+':'+config.https_port;
+          } else {
+            req.hostname = req.hostname;
+            res.locals.hostname = req.hostname;
+          }
+        }
         next();
     };
 };
