1e1145ef38737b62d94bf978d2630668a4728293
Baw Appie <pp121324@gmail.com>
2018-03-18 09:38:52 +0900

    정품인증 사이트는?

--- app.js
+++ app.js
@@ -46,10 +46,11 @@ app.get('/manage/:service/view', function (req, res) {
   if(req.session.user) {
       if(req.params.service == 1) {
         var data = {
-          "name": "후원 사이트",
+          "name": "후원",
           "service": 1
         }
-      //  var korean = ["mail_ok", "sms_ok", "kakao_ok", "tg_ok", "slack_ok"]
+        var list = ["nick", "bal", "method", "pin", "bouns", "ip", "date"];
+        var korean = ["닉네임", "후원 금액", "후원 방법", "핀번호", "원하는 보상", "IP", "날짜"]
 
         var sql_req = sql('select * from page where service=1 and owner=' + SqlString.escape(req.session.user), function(err, rows){
           if (rows.length === 0) {
@@ -58,13 +59,35 @@ app.get('/manage/:service/view', function (req, res) {
     		  }
     		});
         var sql_req2 = sql('select * from service1 where status=0 and owner=' + SqlString.escape(req.session.user), function(err, rows){
-          res.render('manage/view', {rows: rows})
+          res.render('manage/view', {rows: rows, data: data, list: list, korean: korean})
         });
-    }
+      }
+      if(req.params.service == 2) {
+        var data = {
+          "name": "정품 인증",
+          "service": 2
+        }
+        var list = ["nick", "ip", "date"];
+        var korean = ["닉네임", "IP", "날짜"];
+
+        var sql_req = sql('select * from page where service=2 and owner=' + SqlString.escape(req.session.user), function(err, rows){
+          if (rows.length === 0) {
+            req.session.error = '정품인증 페이지가 존재하지 않습니다.';
+            res.redirect('/')
+    		  }
+    		});
+        var sql_req2 = sql('select * from service2 where status=0 and owner=' + SqlString.escape(req.session.user), function(err, rows){
+          res.render('manage/view', {rows: rows, data: data, list: list, korean: korean})
+        });
+      }
   } else {
     res.redirect('/auth/login')
   }
 });
+
+app.get('/manage/:service/complete', function(req, res) {
+  res.redirect('https://baws.kr/edit/view?service=' + req.params.service);
+})
 
 app.post('/manage/:service/edit', function(req, res) {
   if(req.session.user) {
