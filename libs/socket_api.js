5b2e18f3df3a6f5ea657d8417991b50128318d33
Baw Appie <pp121324@gmail.com>
2018-03-20 08:56:36 +0900

    소켓 연결에 문제있다고 화낼 이유 있나요

--- libs/socket_api.js
+++ libs/socket_api.js
@@ -1,6 +1,5 @@
 module.exports = function connectsocket(port,ip,text,callback) {
   var net = require('net');
-
   var client = new net.Socket();
   client.connect(port, ip, function() {
   	client.write('connect;pp121324;say Hello!');
@@ -8,4 +7,5 @@ module.exports = function connectsocket(port,ip,text,callback) {
   });
 
   client.on('data', callback);
+  client.on('error', function(){});
 }
