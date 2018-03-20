083643f68ea53a0a09fe7f598eadc08cf1f7b07e
Baw Appie <pp121324@gmail.com>
2018-03-20 09:24:24 +0900

    고정 메시지라니

--- libs/socket_api.js
+++ libs/socket_api.js
@@ -2,7 +2,7 @@ module.exports = function connectsocket(port,ip,text,callback) {
   var net = require('net');
   var client = new net.Socket();
   client.connect(port, ip, function() {
-  	client.write('connect;pp121324;say Hello!');
+  	client.write(text);
     client.end();
   });
 
