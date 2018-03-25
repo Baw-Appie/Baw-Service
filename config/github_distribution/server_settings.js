1d6e1a05fa96f02c8df1bada214ddaab15f1f22d
Baw Appie <pp121324@gmail.com>
2018-03-25 17:48:09 +0900

    Google Recaptcha 설정

--- config/github_distribution/server_settings.js
+++ config/github_distribution/server_settings.js
@@ -2,6 +2,8 @@ module.exports = (function () {
   return {
     http_port: 80,
     https_port: 443,
-    hostname: "localhost"
+    hostname: "localhost",
+    g_captcha_site_key: "6LeKTiwUAAAAANOYA7r37sTyquy_R0ljzSTVS6Bp",
+    g_captcha_secret_key: "SECRET!"
   }
 })();
