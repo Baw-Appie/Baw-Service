9cf4f916b98b88495c34a41b2663993599766de0
Baw Appie <pp121324@gmail.com>
2018-04-03 18:06:33 +0900

    모바일에서 사이드바 링크 클릭시 자동으로 사이드바 닫힘

--- public/dashboard/js/settings.js
+++ public/dashboard/js/settings.js
@@ -9,3 +9,5 @@ $(document).on('pjax:end',   function() { NProgress.done();  });
 
 
 $(document).on('pjax:error',   function() { toastr.error('Baw Service 페이지 로드 가속을 이용하여 페이지를 로드하는 도중 오류가 발생했습니다.', '죄송합니다!');return false;  });
+
+$('.uk-offcanvas .link a').click(() => { UIkit.offcanvas('#navbar').hide(); })
