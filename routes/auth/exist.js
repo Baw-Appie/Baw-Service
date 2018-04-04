a905dfc4b09265a3a8c923ca2f1ee62267651f7e
Baw Appie <pp121324@gmail.com>
2018-04-04 17:33:45 +0900

    코드 정리

---
+++ routes/auth/exist.js
@@ -0,0 +1,30 @@
+var sql = require('../../config/dbtool');
+var SqlString = require('sqlstring');
+var validator = require('validator');
+module.exports = function(req, res){
+  if(req.params.type){
+    var type = req.params.type
+    if(type == "id"){
+      var sql_Request = SqlString.format('SELECT * FROM id WHERE id=?', [req.body.data])
+    }
+    if(type == "mail"){
+      var sql_Request = SqlString.format('SELECT * FROM id WHERE mail=?', [req.body.data])
+    }
+    var sql_req = sql(sql_Request, function(err, rows){
+      if(err){ throw new Error('1번 질의 오류') }
+      if(rows.length == 0){
+        if(type == "mail"){
+          if(validator.isEmail(req.body.data) == true){
+            res.json({ success: true, message: "사용 가능합니다." })
+          } else {
+            res.json({ success: false, message: "잘못된 이메일 형식입니다." })
+          }
+        } else {
+          res.json({ success: true, message: "사용 가능합니다." })
+        }
+      } else {
+        res.json({ success: false, message: "이미 사용중입니다." })
+      }
+    })
+  }
+}
