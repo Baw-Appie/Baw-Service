f945197044556933be47bd38296e086749b9ca9c
Baw Appie <pp121324@gmail.com>
2018-04-06 20:34:40 +0900

    이메일 서버 셋팅

--- config/github_distribution/server_settings.js
+++ config/github_distribution/server_settings.js
@@ -6,6 +6,9 @@ module.exports = (function () {
     hostname: "localhost",
     g_captcha_site_key: "6LeKTiwUAAAAANOYA7r37sTyquy_R0ljzSTVS6Bp",
     g_captcha_secret_key: "SECRET!",
-    pretty_html: true
+    pretty_html: true,
+    mail_sv: "smtp.worksmobile.com",
+    mail_id: "a-mail-sender@rpgfarm.com",
+    mail_pw: "SECRET"
   }
 })();
