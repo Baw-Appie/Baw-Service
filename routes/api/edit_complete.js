8109c61da5c1dafc88372ab481f8d58832bf28cf
Baw Appie <pp121324@gmail.com>
2018-04-07 20:45:56 +0900

    API 설정 SQL문 수정
    Escape에서 Fotmat으로 전환

--- routes/api/edit_complete.js
+++ routes/api/edit_complete.js
@@ -4,13 +4,25 @@ module.exports = function(req, res) {
     if(req.user) {
         if(req.params.service){
           if(req.params.service == "API") {
-          	var sql_req = sql('UPDATE `id` SET `api_ok` = '+SqlString.escape(req.body.api_ok)+', `api_key`='+SqlString.escape(req.body.api_key)+', `api_ip`='+SqlString.escape(req.body.api_ip)+', `api_port`='+SqlString.escape(req.body.api_port)+', `api`='+SqlString.escape(req.body.api)+' WHERE `id`.`id` = '+ SqlString.escape(req.user.id))
+            var req_field = [api_ok, api_key, api]
+            var opt_field = [api_ip, api_port]
+            var sql_Request = SqlString.format('UPDATE `id` SET `api_ok` = ?, `api_key`=?, `api_ip`=?, `api_port`=?, `api`=? WHERE `id`.`id` =?', [req.body.api_ok, req.body.api_key, req.body.api_ip, req.body.api_port, req.body.api, req.user.id])
+          	var sql_req = sql(sql_Request)
           } else if (req.params.service == "SMS") {
-            var sql_req = sql('UPDATE `sms` SET `phone` = '+SqlString.escape(req.body.phone)+' WHERE `sms`.`id` = '+ SqlString.escape(req.user.id))
+            var req_field = []
+            var opt_field = [phone]
+            var sql_Request = SqlString.format('UPDATE `sms` SET `phone`=? WHERE `sms`.`id` = ?', [req.body.phone, req.user.id])
+            var sql_req = sql(sql_Request)
           } else if (req.params.service == "Telegram") {
-            var sql_req = sql('UPDATE `telegram` SET `chat_id` = '+SqlString.escape(req.body.chat_id)+' WHERE `telegram`.`id` =  '+ SqlString.escape(req.user.id))
+            var req_field = []
+            var opt_field = [chat_id]
+            var sql_Request = SqlString.format('UPDATE `telegram` SET `chat_id` = ? WHERE `telegram`.`id` = ?', [req.body.chat_id, req.user.id])
+            var sql_req = sql(sql_Request)
           } else if (req.params.service == "Custom") {
-            var sql_req = sql('UPDATE `custom_domain` SET `domain` = '+SqlString.escape(req.body.domain)+' WHERE `custom_domain`.`owner` =  '+ SqlString.escape(req.user.id))
+            var req_field = []
+            var opt_field = [domain]
+            var sql_Request = SqlString.format('UPDATE `custom_domain` SET `domain` = ? WHERE `custom_domain`.`id` = ?', [req.body.domain, req.user.id])
+            var sql_req = sql(sql_Request)
           }
           res.json({ success: true, title: "완료했습니다!",  message: "부가 서비스 설정 요청이 전달되었습니다." });
       } else {
