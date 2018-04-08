484996ca32d5ae44e44b6e3a4e80549218c4b85a
Baw Appie <pp121324@gmail.com>
2018-04-08 13:49:50 +0900

    최적화 및 입력 데이터 검증

--- routes/manage/edit_complete.js
+++ routes/manage/edit_complete.js
@@ -1,101 +1,82 @@
 var sql = require('../../config/dbtool');
 var SqlString = require('sqlstring');
-module.exports = function(req, res) {
-    if(req.user) {
-        if(req.params.service){
-          var service = req.params.service;
-        	var notice = req.body.notice;
-        } else {
-        	var notice = "";
-        }
+var vali = require('validator');
 
+function req_check(variable, req){
+  return new Promise(function (resolve, reject) {
+    variable.forEach(function(item) {
+      if(req.body[item] == undefined || req.body[item] == ""){
+        console.log(item)
+        reject(true)
+      }
+    })
+    resolve(true)
+  })
+}
+function opt_check(variable, req){
+  return new Promise(function (resolve, reject) {
+    variable.forEach(function(item) {
+      if(req.body[item] == undefined){
+        reject(true)
+      }
+    })
+    resolve(true)
+  })
+}
+
+module.exports = function(req, res) {
+    if(req.user || req.params.service == 1 || 2 || 3) {
+        var req_field = []
+        var opt_field = ["notice"]
+        var service = req.params.service;
+      	var notice = req.body.notice;
+        if(service == 1){
+          req_field.push("theme", "bouns", "sms_ok", "slack_ok", "tg_ok", "kakao_ok")
+          opt_field.push("youtube", "disabled")
+      		var youtube = req.body.youtube;
+      		var disabled = req.body.disabled.toString();
+      		var theme = "bootstrap3";
+      		var bouns = req.body.bouns;
+      		var sms_ok = req.body.sms_ok;
+      		var slack_ok = req.body.slack_ok;
+      		var tg_ok = req.body.tg_ok;
+      		var kakao_ok = req.body.kakao_ok;
+        }
         if(service == 1 || service  == 2){
-        	if(req.body.mail_ok){
-        		var mail_ok = req.body.mail_ok;
-        	} else {
-        		var mail_ok = 1;
-        	}
-        	if(req.body.api_cmd) {
-        		var api_cmd = req.body.api_cmd;
-        	} else {
-        		var api_cmd = "";
-        	}
+          req_field.push("mail_ok")
+          opt_field.push("api_cmd")
+      		var mail_ok = req.body.mail_ok;
+      		var api_cmd = req.body.api_cmd;
         }
         if(service == 2){
-        	if(req.body.auto_process){
-        		var auto_process = req.body.auto_process;
-        	} else {
-        		var auto_process = 1;
-        	}
-        }
-
-        if(service == 1){
-        	if(req.body.youtube){
-        		var youtube = req.body.youtube;
-        	} else {
-        		var youtube = "";
-        	}
-        	if(req.body.disabled){
-        		var disable = req.body.disabled;
-        	} else {
-        		var disable = "";
-        	}
-        	if(req.body.theme){
-        		var theme = req.body.theme;
-        	} else {
-        		var theme = "bootstrap3";
-        	}
-        	var disabled = "계좌이체";
-        	if(req.body.bouns){
-        		var bouns = req.body.bouns;
-        	} else {
-        		var bouns = "";
-        	}
-        	if(req.body.sms_ok){
-        		var sms_ok = req.body.sms_ok;
-        	} else {
-        		var sms_ok = 0;
-        	}
-        	if(req.body.slack_ok){
-        		var slack_ok = req.body.slack_ok;
-        	} else {
-        		var slack_ok = 0;
-        	}
-        	if(req.body.tg_ok){
-        		var tg_ok = req.body.tg_ok;
-        	} else {
-        		var tg_ok = 0;
-        	}
-        	if(req.body.kakao_ok){
-        		var kakao_ok = req.body.kakao_ok;
-        	} else {
-        		var kakao_ok = 0;
-        	}
+          req_field.push("auto_process")
+      		var auto_process = req.body.auto_process;
         }
         if(service == 3){
-        	if(req.body.sv_ip){
-        		var sv_ip = req.body.sv_ip;
-        	} else {
-        		var sv_ip = "";
-        	}
-        	if(req.body.sv_port){
-        		var sv_port = req.body.sv_port;
-        	} else {
-        		var sv_port = "";
-        	}
+          req_field.push("sv_ip", "sv_port")
+      		var sv_ip = req.body.sv_ip;
+      		var sv_port = req.body.sv_port;
         }
 
-        if(req.params.service == 1) {
-          var sql_req = sql('UPDATE `page` SET `mail_ok` = ' + SqlString.escape(mail_ok) + ', `bouns` = ' + SqlString.escape(bouns) + ', `sms_ok` = ' + SqlString.escape(sms_ok) + ', `slack_ok` = ' + SqlString.escape(slack_ok) + ', `kakao_ok` = ' + SqlString.escape(kakao_ok) + ', `tg_ok` = ' + SqlString.escape(tg_ok) + ', `api_cmd` = ' + SqlString.escape(api_cmd) + ', `disabled`=' + SqlString.escape(disabled) + ', `notice` =  ' + SqlString.escape(notice) +', `theme`=' + SqlString.escape(theme) + ', `youtube`=' + SqlString.escape(youtube) + ' WHERE service=1 and owner=' + SqlString.escape(req.user.id))
-        }
-
-        if(req.params.service == 2) {
-        	var sql_req = sql("UPDATE `page` SET `mail_ok` = " + SqlString.escape(mail_ok) + ", `api_cmd` = " + SqlString.escape(api_cmd) +", `notice`=" + SqlString.escape(notice) + ", `youtube`=" + SqlString.escape(youtube) + " , `auto_process`=" + SqlString.escape(auto_process) + " WHERE service=2 and owner=" + SqlString.escape(req.user.id))
-        }
-        if(req.params.service == 3) {
-        	var sql_req = sql("UPDATE `page` SET `sv_ip` = " + SqlString.escape(sv_ip) + ", `sv_port` = " + SqlString.escape(sv_port) + ", `notice`=" + SqlString.escape(notice) + ", `youtube`=" + SqlString.escape(youtube) + " WHERE service=3 and owner=" + SqlString.escape(req.user.id))
-        }
-        res.json({ success: true, title: "완료했습니다!",  message: "정상적으로 해당 페이지에 적용되었습니다." });
+        req_check(req_field, req).then(function (text) {
+          opt_check(opt_field, req).then(function (text) {
+            if(req.params.service == 1) {
+              var sql_Request = SqlString.format('UPDATE `page` SET `mail_ok` = ?, `bouns` = ?, `sms_ok` = ?, `slack_ok` =  ?, `kakao_ok` = ?, `tg_ok` = ?, `api_cmd` = ?, `disabled`=?, `notice` =?, `theme`=?, `youtube`=? WHERE service=1 and owner=?', [mail_ok, bouns, sms_ok, slack_ok, kakao_ok, tg_ok, api_cmd, disabled, notice, theme, youtube, req.user.id])
+            }
+            if(req.params.service == 2) {
+            	var sql_Request = SqlString.format("UPDATE `page` SET `mail_ok` =?, `api_cmd` = ?, `notice`=?, `youtube`=? , `auto_process`=? WHERE service=2 and owner=?", [mail_ok, api_cmd, notice, youtube, auto_process, req.user.id])
+            }
+            if(req.params.service == 3) {
+            	var sql_Request = SqlString.format("UPDATE `page` SET `sv_ip` = ?, `sv_port` = ?, `notice`=?, `youtube`=? WHERE service=3 and owner=?", [sv_ip, sv_port, notice, youtube, req.user.id])
+            }
+            var sql_req = sql(sql_Request)
+            res.json({ success: true, title: "완료했습니다!",  message: "정상적으로 해당 페이지에 적용되었습니다." });
+          }).catch(function (error) {
+            res.json({ success: false, title: "필요 데이터 미전달됨",  message: "설정에 필요한 데이터가 전달되지 않았습니다." });
+          });
+        }).catch(function (error) {
+          res.json({ success: false, title: "필요 데이터 미전달됨",  message: "설정에 필요한 데이터가 전달되지 않았거나 설정되지 않았습니다." });
+        });
     } else {
       res.json({ success: false, title: "권한이 없습니다.",  message: "페이지 수정 권한이 없습니다." });
     }
