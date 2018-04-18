6c5557e6ade13b329683cb56b40a005b557fd158
Baw_Appie <pp121324@gmail.com>
2018-04-18 20:56:32 +0900

    사이트 설정 변경 및 최적화

--- public/dashboard/js/settings.js
+++ public/dashboard/js/settings.js
@@ -6,13 +6,28 @@ window.onload = function () {
 $( document ).pjax( 'a', '#contents' );
 $(document).on('pjax:start', function() { NProgress.start(); });
 $(document).on('pjax:end',   function() { NProgress.done();  });
+$(document).on('pjax:error',   function() { toastr.error('Baw Service 페이지 로드 가속을 이용하여 페이지를 로드하는 도중 오류가 발생했습니다.', '죄송합니다!'); return false;  });
+$.pjax.defaults.timeout = 1200
 
-
-$(document).on('pjax:error',   function() { toastr.error('Baw Service 페이지 로드 가속을 이용하여 페이지를 로드하는 도중 오류가 발생했습니다.', '죄송합니다!');return false;  });
+toastr.options = {
+		  "closeButton": false,
+		  "debug": false,
+		  "newestOnTop": false,
+		  "progressBar": false,
+		  "positionClass": "toast-bottom-right",
+		  "preventDuplicates": false,
+		  "onclick": null,
+		  "showDuration": "300",
+		  "hideDuration": "1000",
+		  "timeOut": "5000",
+		  "extendedTimeOut": "1000",
+		  "showEasing": "swing",
+		  "hideEasing": "linear",
+		  "showMethod": "fadeIn",
+		  "hideMethod": "fadeOut"
+		}
 
 $('.uk-offcanvas .link a').click(() => { UIkit.offcanvas('#navbar').hide(); })
-
-
 $('.link a').click(() => {
   $(".link a").removeClass('uk-active');
   $(this).addClass('uk-active');
