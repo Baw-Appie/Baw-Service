07cd3f08f1b66987e59e439f80c853e88f073302
Baw Appie <pp121324@gmail.com>
2018-04-22 10:15:00 +0900

    심각한 커스텀 도메인 버그 수정

--- libs/custom_domains.js
+++ libs/custom_domains.js
@@ -3,7 +3,7 @@ var server_settings = require('../config/server_settings');
 var SqlString = require('sqlstring');
 module.exports = function() {
     return function custom_domains( req, res, next ) {
-      if(req.path == "/") {
+      if(req.path == "/" && req.hostname != server_settings.hostname) {
         var sql_req = sql.query('SELECT * FROM custom_domain WHERE domain='+SqlString.escape(req.hostname), function(err, rows){
           if(err) { throw new Error('1번 질의 오류') }
           if(rows.length == 1){
