e46c93d31b4e2c63ae7bbb99c528531f742744f9
Baw Appie <pp121324@gmail.com>
2018-04-26 18:21:22 +0900

    Excel로 내보내기 완성

--- routes/manage/data_export.js
+++ routes/manage/data_export.js
@@ -1,173 +1,163 @@
 var sql = require('../../config/dbtool');
 var SqlString = require('sqlstring');
+var request = require('request');
+var server_settings = require('../../config/server_settings');
+
+function captcha(req){
+  return new Promise(function (resolve, reject) {
+    if(req.query['g-recaptcha-response'] === undefined || req.query['g-recaptcha-response'] === '' || req.query['g-recaptcha-response'] === null) {
+      return reject('Recaptcha 인증에 필요한 데이터가 부족합니다.')
+    }
+    var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + server_settings.g_captcha_secret_key + "&response=" + req.query['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+    request(verificationUrl, function(error,response,body) {
+      body = JSON.parse(body);
+      if(body.success !== undefined && !body.success) {
+        return reject('Recaptcha 인증에 실패하였습니다.')
+      }
+    })
+    resolve(true)
+  })
+}
+
 module.exports = function (req, res) {
     if(req.user) {
-      if(req.params.service == 1) {
-        const excel = require('node-excel-export');
-        const styles = {
-          headerDark: {
-            fill: {
-              fgColor: {
-                rgb: 'FF000000'
-              }
-            },
-            font: {
-              color: {
-                rgb: 'FFFFFFFF'
+      captcha(req, res).then(function (text) {
+        if(req.params.service == 1) {
+          const excel = require('node-excel-export');
+          const styles = {
+            headerDark: {
+              fill: {
+                fgColor: {
+                  rgb: 'FF000000'
+                }
               },
-              sz: 14,
-              bold: true,
-              underline: true
-            }
-          },
-          cellPink: {
-            fill: {
-              fgColor: {
-                rgb: 'FFFFCCFF'
+              font: {
+                color: {
+                  rgb: 'FFFFFFFF'
+                },
+                sz: 14,
+                bold: true,
+                underline: true
+              }
+            },
+            cellPink: {
+              fill: {
+                fgColor: {
+                  rgb: 'FFFFCCFF'
+                }
+              }
+            },
+            cellGreen: {
+              fill: {
+                fgColor: {
+                  rgb: 'FF00FF00'
+                }
+              }
+            },
+            cellRed: {
+              fill: {
+                fgColor: {
+                  rgb: 'FFFF0000'
+                }
               }
             }
-          },
-          cellGreen: {
-            fill: {
-              fgColor: {
-                rgb: 'FF00FF00'
-              }
+          };
+          const heading = [
+            [{value: 'Baw Service 후원 로그', style: styles.headerDark}]
+          ];
+          const specification = {
+            num: {
+              displayName: '번호',
+              headerStyle: styles.headerDark,
+              cellStyle: styles.cellPink,
+              width: 40
+            },
+            status: {
+              displayName: '상태',
+              headerStyle: styles.headerDark,
+              cellStyle: function(value, row) {
+                if(row.status == 1) { return styles.cellGreen } else { return styles.cellRed }
+              },
+              width: 40
+            },
+            pin: {
+              displayName: '핀번호',
+              headerStyle: styles.headerDark,
+              cellStyle: styles.cellPink,
+              width: 220
+            },
+            bal: {
+              displayName: '금액',
+              headerStyle: styles.headerDark,
+              cellStyle: styles.cellPink,
+              width: 100
+            },
+            method: {
+              displayName: '후원 방법',
+              headerStyle: styles.headerDark,
+              cellStyle: styles.cellPink,
+              width: 100
+            },
+            code: {
+              displayName: '발행일(인증번호)',
+              headerStyle: styles.headerDark,
+              cellStyle: styles.cellPink,
+              width: 100
+            },
+            nname: {
+              displayName: '입금자명',
+              headerStyle: styles.headerDark,
+              cellStyle: styles.cellPink,
+              width: 100
+            },
+            bouns: {
+              displayName: '후원 보너스',
+              headerStyle: styles.headerDark,
+              cellStyle: styles.cellPink,
+              width: 220
+            },
+            ip: {
+              displayName: 'IP',
+              headerStyle: styles.headerDark,
+              cellStyle: styles.cellPink,
+              width: 150
+            },
+            date: {
+              displayName: '날짜',
+              headerStyle: styles.headerDark,
+              cellStyle: styles.cellPink,
+              width: 100
             }
           }
-        };
-        const heading = [
-          [{value: 'Baw Service 후원 로그', style: styles.headerDark}]
-        ];
-        const specification = {
-          customer_name: {
-            displayName: 'Customer',
-            headerStyle: styles.headerDark,
-            cellStyle: function(value, row) {
-              return (row.status_id == 1) ? styles.cellGreen : {fill: {fgColor: {rgb: 'FFFF0000'}}};
-            },
-            width: 120
-          },
-          status_id: {
-            displayName: 'Status',
-            headerStyle: styles.headerDark,
-            cellFormat: function(value, row) {
-              return (value == 1) ? 'Active' : 'Inactive';
-            },
-            width: '10'
-          },
-          note: {
-            displayName: 'Description',
-            headerStyle: styles.headerDark,
-            cellStyle: styles.cellPink,
-            width: 220
-          }
-        }
-
-        // The data set should have the following shape (Array of Objects)
-        // The order of the keys is irrelevant, it is also irrelevant if the
-        // dataset contains more fields as the report is build based on the
-        // specification provided above. But you should have all the fields
-        // that are listed in the report specification
-        const dataset = [
-          {customer_name: 'IBM', status_id: 1, note: 'some note', misc: 'not shown'},
-          {customer_name: 'HP', status_id: 0, note: 'some note'},
-          {customer_name: 'MS', status_id: 0, note: 'some note', misc: 'not shown'}
-        ]
-        // The data set should have the following shape (Array of Objects)
-
-        // Define an array of merges. 1-1 = A:1
-        // The merges are independent of the data.
-        // A merge will overwrite all data _not_ in the top-left cell.
-        const merges = [
-          { start: { row: 1, column: 1 }, end: { row: 1, column: 10 } },
-          { start: { row: 2, column: 1 }, end: { row: 2, column: 5 } },
-          { start: { row: 2, column: 6 }, end: { row: 2, column: 10 } }
-        ]
-
-        // Create the excel report.
-        // This function will return Buffer
-        const report = excel.buildExport(
-          [ // <- Notice that this is an array. Pass multiple sheets to create multi sheet report
-            {
-              name: 'Report', // <- Specify sheet name (optional)
-              heading: heading, // <- Raw heading array (optional)
-              merges: merges, // <- Merge cell ranges
-              specification: specification, // <- Report specification
-              data: dataset // <-- Report data
-            }
+          const merges = [
+            { start: { row: 1, column: 1 }, end: { row: 1, column: 10 } }
           ]
-        );
 
-        // You can then return this straight
-        res.attachment('report.xlsx'); // This is sails.js specific (in general you need to set headers)
-        res.send(report);
-        // var nodeExcel=require('excel-export');
-        // var conf={}
-        // conf.cols=[
-        //   {
-        //     caption:'등록 번호',
-        //     type:'number'
-        //   },
-        //   {
-        //     caption:'닉네임',
-        //     type:'string'
-        //   },
-        //   {
-        //     caption:'후원 금액',
-        //     type:'string'
-        //   },
-        //   {
-        //     caption:'핀번호',
-        //     type:'string'
-        //   },
-        //   {
-        //     caption:'후원 방법',
-        //     type:'string'
-        //   },
-        //   {
-        //     caption:'발행일(인증번호)',
-        //     type:'string'
-        //   },
-        //   {
-        //     caption:'입금자명',
-        //     type:'string'
-        //   },
-        //   {
-        //     caption:'후원 보너스',
-        //     type:'string'
-        //   },
-        //   {
-        //     caption:'IP',
-        //     type:'string'
-        //   },
-        //   {
-        //     caption:'날짜',
-        //     type:'string'
-        //   },
-        //   {
-        //     caption:'처리 상태',
-        //     type:'string'
-        //   }
-        // ];
-        // var query=sql.query("select * from service1 where owner="+SqlString.escape(req.user.id), function(err,rows){
-        //   if(err){ throw new Error('1번 질의 오류') }
-        //   arr=[];
-        //   for(i=0;i<rows.length;i++){
-        //     a=[rows[i]['num'], rows[i]['nick'], rows[i]['bal'], rows[i]['pin'], rows[i]['method'], rows[i]['code'], rows[i]['nname'], rows[i]['bouns'], rows[i]['ip'], rows[i]['date'], rows[i]['status']];
-        //     arr.push(a);
-        //   }
-        //   conf.rows=arr;
-        //   console.log(conf)
-        //   var result=nodeExcel.execute(conf);
-        //   res.setHeader('Content-Type','application/vnd.openxmlformates');
-        //   res.setHeader("Content-Disposition","attachment;filename="+"Powered by Baw Service.xlsx");
-        //   res.end(result,'binary');
-        // });
-      } else if(req.params.service == 2) {
-        // TODO: 정품인증 데이터 내보내기
-      } else {
-        res.render('error/403')
-      }
+          var query=sql.query("select * from service1 where owner="+SqlString.escape(req.user.id), function(err,rows){
+            if(err){ throw new Error('1번 질의 오류') }
+            const report = excel.buildExport(
+              [
+                {
+                  name: 'Report',
+                  heading: heading,
+                  merges: merges,
+                  specification: specification,
+                  data: rows
+                }
+              ]
+            );
+            res.attachment('report.xlsx');
+            res.send(report);
+            console.log('gd')
+          });
+        } else if(req.params.service == 2) {
+          // TODO: 정품인증 데이터 내보내기
+        } else {
+          res.render('error/403')
+        }
+      }).catch(function (error) {
+      	res.json({ success: false, message: error })
+      });
     } else {
       res.redirect('/auth/login')
     }
