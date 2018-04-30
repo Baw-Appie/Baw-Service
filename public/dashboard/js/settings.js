f30d6a08ed9405f771763e215a67a87d35d9204c
Baw Appie <pp121324@gmail.com>
2018-04-30 17:56:51 +0900

    iziToast 적용

--- public/dashboard/js/settings.js
+++ public/dashboard/js/settings.js
@@ -1,12 +1,7 @@
-window.onload = function () {
-  //var url = location.href
-  //$.pjax({url: url, container: '#pjax'})
-  //toastr.info('Baw Service Dashboard Beta 4 초기 설정중..', '환영합니다!');
-}
 $( document ).pjax( 'a', '#contents' );
 $(document).on('pjax:start', function() { NProgress.start(); });
 $(document).on('pjax:end',   function() { NProgress.done();  });
-$(document).on('pjax:error',   function() { toastr.error('Baw Service 페이지 로드 가속을 이용하여 페이지를 로드하는 도중 오류가 발생했습니다.', '죄송합니다!'); return false;  });
+$(document).on('pjax:error',   function() { iziToast.error({title: '이런..', message: 'Baw Service 페이지 로드 가속을 이용하여 페이지를 로드하는 도중 오류가 발생했습니다.'}); return false;  });
 $.pjax.defaults.timeout = 1200
 
 toastr.options = {
