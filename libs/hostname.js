dbc40faeaf82f7d3f3b5d1ab81671853b530c95d
Baw Appie <pp121324@gmail.com>
2018-05-04 13:12:33 +0900

    fix port auto bind

--- libs/hostname.js
+++ libs/hostname.js
@@ -10,12 +10,12 @@ module.exports = function() {
             res.locals.hostname = req.hostname;
           }
         } else {
-          if (config.https_port == 443 && config.port_autobind == false) {
-            req.hostname = req.hostname;
-            res.locals.hostname = req.hostname;
-          } else {
+          if (config.https_port !== 443 && config.port_autobind == true) {
             req.hostname = req.hostname+':'+config.https_port;
             res.locals.hostname = req.hostname+':'+config.https_port;
+          } else {
+            req.hostname = req.hostname;
+            res.locals.hostname = req.hostname;
           }
         }
         next();
