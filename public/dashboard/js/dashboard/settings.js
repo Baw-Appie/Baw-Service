8df56c925f3ecaa219bda6eb02d3eeca200e686e
Baw-Appie <pp121324@gmail.com>
2018-06-16 16:51:20 +0900

    Google Smart Lock지원

--- public/dashboard/js/dashboard/settings.js
+++ public/dashboard/js/dashboard/settings.js
@@ -8,3 +8,20 @@ $('.link a').click(() => {
   $(".link a").removeClass('uk-active');
   $(this).addClass('uk-active');
 })
+function post(path, params, method) {
+  method = method || "post";
+  var form = document.createElement("form");
+  form.setAttribute("method", method);
+  form.setAttribute("action", path);
+  for(var key in params) {
+    if(params.hasOwnProperty(key)) {
+      var hiddenField = document.createElement("input");
+      hiddenField.setAttribute("type", "hidden");
+      hiddenField.setAttribute("name", key);
+      hiddenField.setAttribute("value", params[key]);
+      form.appendChild(hiddenField);
+    }
+  }
+  document.body.appendChild(form);
+  form.submit();
+}
