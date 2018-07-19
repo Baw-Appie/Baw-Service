8581635ddcf789ad4f3238ac53b6b6d47617e60f
Baw-Appie <pp121324@gmail.com>
2018-07-19 19:21:03 +0900

    resovled #53

--- routes/manage/complete.js
+++ routes/manage/complete.js
@@ -17,38 +17,40 @@ module.exports = function(req, res) {
             if(err) { throw err };
             if(!req.query.noapi && req.params.status == "1"){
               sql.query(SqlString.format("SELECT * from `api` WHERE id=?", [req.user.id]), function(err, rows4){
-                if(req.params.service == "1"){
-                  if(rows4[0]['api_enable'] == 1) {
-                    sql.query('select * from `pages` WHERE service=1 and owner='+SqlString.escape(req.user.id), function(err, rows3) {
-                      if(err) { throw err };
-                      var api_cmd = JSON.parse(rows3[0]['pagedata'])['api_cmd'];
-                      api_cmd = api_cmd.replace("<player>", rows[0]['nick']);
-                      api_cmd = api_cmd.replace("<money>", rows[0]['bal']);
-                      api_cmd = api_cmd.replace("<package>", rows[0]['bouns']);
-                      api_cmd = api_cmd.replace("원", "");
-                      api_cmd = api_cmd.replace(",", "");
-                      if(rows2[0]['api'] == "socket"){
-                        socket_api(rows4[0]['api_port'], rows4[0]['api_ip'], rows4[0]['api_key']+';'+rows2[0]['id']+';'+api_cmd, function(data){});
-                      }
-                      if(rows2[0]['api'] == "HTTP") {
-                        sql.query('insert into api1 values ("'+req.user.id+'", '+SqlString.escape(rows4[0]['api_key'])+', '+SqlString.escape(rows[0]['page'])+', '+SqlString.escape(rows[0]['nick'])+', '+SqlString.escape(rows[0]['bal'])+', '+SqlString.escape(rows[0]['pin'])+', '+SqlString.escape(api_cmd)+')')
-                      }
-                    })
-                  }
-                } else if (req.params.service == "2"){
-                  if(rows4[0]['api_enable'] == 1) {
-                    sql.query('select * from `pages` WHERE service=2 and owner='+SqlString.escape(req.user.id), function(err, rows3) {
-                      if(err) { throw err };
-                      var api_cmd = JSON.parse(rows3[0]['pagedata'])['api_cmd'];
-                      api_cmd = api_cmd.replace("<player>", rows[0]['nick']);
-                      if(rows2[0]['api'] == "socket"){
-                        socket_api(rows4[0]['api_port'], rows4[0]['api_ip'], rows4[0]['api_key']+';'+rows2[0]['id']+';'+api_cmd, function(data){});
-                      }
-                      if(rows2[0]['api'] == "HTTP") {
-                        var sql_Request = SqlString.format('insert into api2 values (?, ?, ?, ?, ?)', [req.user.id, rows4[0]['api_key'], rows[0]['page'], rows[0]['nick'], api_cmd])
-                        sql.query(sql_Request)
-                      }
-                    })
+                if(rows4.length != 0){
+                  if(req.params.service == "1"){
+                    if(rows4[0]['api_enable'] == 1) {
+                      sql.query('select * from `pages` WHERE service=1 and owner='+SqlString.escape(req.user.id), function(err, rows3) {
+                        if(err) { throw err };
+                        var api_cmd = JSON.parse(rows3[0]['pagedata'])['api_cmd'];
+                        api_cmd = api_cmd.replace("<player>", rows[0]['nick']);
+                        api_cmd = api_cmd.replace("<money>", rows[0]['bal']);
+                        api_cmd = api_cmd.replace("<package>", rows[0]['bouns']);
+                        api_cmd = api_cmd.replace("원", "");
+                        api_cmd = api_cmd.replace(",", "");
+                        if(rows2[0]['api'] == "socket"){
+                          socket_api(rows4[0]['api_port'], rows4[0]['api_ip'], rows4[0]['api_key']+';'+rows2[0]['id']+';'+api_cmd, function(data){});
+                        }
+                        if(rows2[0]['api'] == "HTTP") {
+                          sql.query('insert into api1 values ("'+req.user.id+'", '+SqlString.escape(rows4[0]['api_key'])+', '+SqlString.escape(rows[0]['page'])+', '+SqlString.escape(rows[0]['nick'])+', '+SqlString.escape(rows[0]['bal'])+', '+SqlString.escape(rows[0]['pin'])+', '+SqlString.escape(api_cmd)+')')
+                        }
+                      })
+                    }
+                  } else if (req.params.service == "2"){
+                    if(rows4[0]['api_enable'] == 1) {
+                      sql.query('select * from `pages` WHERE service=2 and owner='+SqlString.escape(req.user.id), function(err, rows3) {
+                        if(err) { throw err };
+                        var api_cmd = JSON.parse(rows3[0]['pagedata'])['api_cmd'];
+                        api_cmd = api_cmd.replace("<player>", rows[0]['nick']);
+                        if(rows2[0]['api'] == "socket"){
+                          socket_api(rows4[0]['api_port'], rows4[0]['api_ip'], rows4[0]['api_key']+';'+rows2[0]['id']+';'+api_cmd, function(data){});
+                        }
+                        if(rows2[0]['api'] == "HTTP") {
+                          var sql_Request = SqlString.format('insert into api2 values (?, ?, ?, ?, ?)', [req.user.id, rows4[0]['api_key'], rows[0]['page'], rows[0]['nick'], api_cmd])
+                          sql.query(sql_Request)
+                        }
+                      })
+                    }
                   }
                 }
               })
