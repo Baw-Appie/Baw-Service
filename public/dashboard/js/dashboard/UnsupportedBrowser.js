121481499adace2b8583f0aa5c18d0c55e38de7f
Baw_Appie <pp121324@gmail.com>
2018-10-04 20:24:12 +0900

    FireFox 정식 지원 추가 #62

--- public/dashboard/js/dashboard/UnsupportedBrowser.js
+++ public/dashboard/js/dashboard/UnsupportedBrowser.js
@@ -1,4 +1,4 @@
-if(browser.indexOf("Chrome") == -1 && browser.indexOf("Edge") == -1 && browser.indexOf("Safari") == -1){
+if(browser.indexOf("Chrome") == -1 && browser.indexOf("Edge") == -1 && browser.indexOf("Safari") == -1 && browser.indexOf("Firefox") == -1){
   iziToast.warning({ title: "미지원 브라우저 사용중", message: "현재 미지원 브라우저를 사용하고 있습니다. 최적의 환경을 위하여 Safari 또는 Chrome으로 접속하세요." })
   if(browser.indexOf("IE") != -1){
     if(location.href.indexOf("UnsupportedBrowser") == -1){
