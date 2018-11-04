958d5dfedc4438568e8e3074cadfee6ebea82e23
Baw_Appie <pp121324@gmail.com>
2018-11-04 14:50:42 +0900

    버그 수정 & 사이트 최적화

--- routes/secuity/complete_allow_katalk.js
+++ routes/secuity/complete_allow_katalk.js
@@ -9,23 +9,24 @@ module.exports = async (req, res) => {
       var success = true
       var { phone, code } = req.body
       if (vali.isEmpty(code)) {
-        thorw ('인증번호를 입력해주세요')
+        throw ('인증번호를 입력해주세요')
       }
       if (vali.isEmpty(phone)) {
-        thorw ('ID를 입력해주세요.')
+        throw ('전화번호를 입력해주세요.')
       }
-      if ((await sqlp(sql, "SELECT * FROM katalk WHERE id=?", [req.user.id])).length != 0) {
-        thorw ('이미 카카오톡 알림 서비스에 등록되어 있습니다.')
+      if ((await sqlp(sql, SqlString.format("SELECT * FROM katalk WHERE id=?", [req.user.id]))).length != 0) {
+        throw ('이미 카카오톡 알림 서비스에 등록되어 있습니다.')
       }
-      var data = await sqlp(sql, "SELECT * FROM 2fa WHERE id=? and phone=?", [req.user.id, phone])
+      var data = await sqlp(sql, SqlString.format("SELECT * FROM 2fa WHERE phone=?", [phone]))
       if (data.length == 0) {
-        thorw ('인증번호를 요청한적이 없습니다.')
+        throw ('인증번호를 요청한적이 없습니다.')
       }
-      if (data[0]['code']) {
-        thorw ('인증번호가 일치하지 않습니다. 인증번호를 다시 확인하세요.')
+      if (data[0]['code'] != code) {
+        throw ('인증번호가 일치하지 않습니다. 인증번호를 다시 확인하세요.')
       }
       var sql_Request = SqlString.format("INSERT INTO katalk values (?, ?, 0)", [req.user.id, phone, 0])
       try { await sqlp(sql, sql_Request) } catch { return res.json({ success: false, title: '실패했습니다.', message: "요청에 실패했습니다. 좌측 메뉴의 버그 신고로 이 문제를 신고하세요." }) }
+      var msg = "카카오톡 알림톡 서비스 가입에 성공했습니다!"
     } catch (err) {
       console.log(err)
       if (err instanceof Error) {
--- routes/secuity/req_code.js
+++ routes/secuity/req_code.js
@@ -1,6 +1,10 @@
-var send2fa = require('../../libs/send2fa');
+var send2fa = require('../../libs/send2fa')
+var sql = require('../../config/dbtool')
+var sqlp = require('../../libs/sql-promise')
+var SqlString = require('SqlString')
+
 module.exports = async (req, res) => {
-  if(req.user && req.body.phone) {
+  if(req.user && req.body.phone && (await sqlp(sql, SqlString.format("SELECT * FROM katalk WHERE id=?", [req.user.id]))).length == 0) {
     try {
       res.json(await send2fa(req.user.id, req.body.phone, req.ip))
     } catch(e) {
