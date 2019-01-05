b086b06623e0ffea80b30b66a9da3445e58a6675
Baw Appie <pp121324@gmail.com>
2019-01-05 10:45:33 +0900

    커스텀 도메인 버그 수정

--- libs/custom_domains.js
+++ libs/custom_domains.js
@@ -7,9 +7,8 @@ module.exports = async (req, res, next) => {
   if(req.path == "/" && req.hostname != server_settings.hostname) {
     var data = await sqlp(sql, SqlString.format("SELECT * FROM custom_domain WHERE ?", [{domain: req.hostname}]))
     if(data.length == 1){
-      return require('./userpage-with-404')(req, res, next, data[0]['go'])
-      // return require('../routes/userpage=with-404')(req, res, next, data[0]['go'])
-      // return res.render('user_page/custom_domain', {url: data[0]['go'], hostname: server_settings.hostname})
+      // return require('./userpage-with-404')(req, res, next, data[0]['go'])
+      return res.render('user_page/custom_domain', {url: data[0]['go'], hostname: server_settings.hostname})
     }
   }
   next()
