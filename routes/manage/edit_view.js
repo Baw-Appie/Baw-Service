6a2dd499ea0ebfb0d74e707aa35c1ecefbd5301c
Baw_Appie <pp121324@gmail.com>
2019-01-06 15:54:20 +0900

    코드 최적화

--- routes/manage/edit_view.js
+++ routes/manage/edit_view.js
@@ -1,8 +1,9 @@
 var sql = require('../../config/dbtool');
+var sqlp = require('../../libs/sql-promise');
 var SqlString = require('sqlstring');
-module.exports = (req, res) => {
-  if(req.user) {
-    if(req.params.service == 1) {
+module.exports = async (req, res) => {
+  if (req.user) {
+    if (req.params.service == 1) {
       var data = {
         "name": "후원 사이트",
         "service": 1
@@ -14,13 +15,13 @@ module.exports = (req, res) => {
             description: "사이트 일반 설정",
             select: [{ name: "lookup_ok", korean: "후원 기록 조회 허용" }],
             text: [
-              { name: "bouns", korean: "후원 보너스"},
-              { name: "youtube", korean: "Youtube Video ID"},
-              { name: "background", korean: "백그라운드 이미지 URL"}
+              { name: "bouns", korean: "후원 보너스" },
+              { name: "youtube", korean: "Youtube Video ID" },
+              { name: "background", korean: "백그라운드 이미지 URL" }
             ],
             textarea: [{ name: "notice", korean: "공지사항" }],
             custom_select: [{ name: "theme", korean: "테마 (베타)", option_data: ["semanticui", "bootstrap3", "uikit", "bootstrap4", "material"], option_korean: ["Semantic UI", "Bootstrap3", "UIKit", "Bootstrap4", "Material (Beta)"] }],
-            custom_checkbox: [{name: "disabled", korean: "사용하지 않을 후원 방법", option_data: ["문화상품권1", "도서문화상품권", "해피머니", "틴캐시", "계좌이체"], option_korean: ["문화상품권", "도서 문화상품권", "해피머니", "틴캐시", "계좌이체"]}]
+            custom_checkbox: [{ name: "disabled", korean: "사용하지 않을 후원 방법", option_data: ["문화상품권1", "도서문화상품권", "해피머니", "틴캐시", "계좌이체"], option_korean: ["문화상품권", "도서 문화상품권", "해피머니", "틴캐시", "계좌이체"] }]
           },
           "integration": {
             korean: "사이트 서비스 통합",
@@ -30,25 +31,21 @@ module.exports = (req, res) => {
               { name: "sms_ok", korean: "SMS 알림" },
               { name: "kakao_ok", korean: "카카오톡 알림" },
               { name: "tg_ok", korean: "텔레그램 알림" },
-              { name: "browser_ok", korean: "브라우저 푸쉬 알림"},
+              { name: "browser_ok", korean: "브라우저 푸쉬 알림" },
             ],
             text: [{ name: "api_cmd", korean: "API 플러그인 명령어" }]
           }
         },
-        "savetojson": ["mail_ok", "sms_ok", "kakao_ok", "tg_ok", "browser_ok", "lookup_ok", "bouns","api_cmd","youtube", "disabled", "background"]
+        "savetojson": ["mail_ok", "sms_ok", "kakao_ok", "tg_ok", "browser_ok", "lookup_ok", "bouns", "api_cmd", "youtube", "disabled", "background"]
       }
       var help = "openlink:https://baw-service.tistory.com/40"
-
-      sql.query('select * from pages where service=1 and owner=' + SqlString.escape(req.user.id), function(err, rows){
-        if (rows.length === 0) {
-          req.session.error = '후원 홈페이지가 존재하지 않습니다. 먼저 페이지를 생성해주세요!';
-          res.redirect('/manage')
-        } else {
-          var pagedata = JSON.parse(rows[0]['pagedata'])
-          res.render('manage/edit', { data: data, rows: rows, pagedata: pagedata, help: help, options: options })
-        }
-      });
-    } else if(req.params.service == 2) {
+      var rows = await sqlp(sql, SqlString.format("SELECT * FROM pages WHERE service=1 AND owner=?", [req.user.id]))
+      if (rows.length == 0) {
+        req.session.error = '후원 홈페이지가 존재하지 않습니다. 먼저 페이지를 생성해주세요!';
+        return res.redirect('/manage')
+      }
+      res.render('manage/edit', { data: data, rows: rows, pagedata: JSON.parse(rows[0]['pagedata']), help: help, options: options })
+    } else if (req.params.service == 2) {
       var data = {
         "name": "정품 인증 사이트",
         "service": 2
@@ -65,7 +62,7 @@ module.exports = (req, res) => {
             korean: "사이트 서비스 통합",
             description: "알림 / API 서비스 통합",
             text: [
-              { name: "api_cmd", korean: "API 플러그인 명령어"}
+              { name: "api_cmd", korean: "API 플러그인 명령어" }
             ],
             select: [
               { name: "mail_ok", korean: "메일 알림" }
@@ -75,18 +72,13 @@ module.exports = (req, res) => {
         "savetojson": ["mail_ok", "auto_process", "api_cmd"]
       }
       var help = "openlink:https://baw-service.tistory.com/41"
-
-      sql.query('select * from pages where service=2 and owner=' + SqlString.escape(req.user.id), function(err, rows){
-        if (rows.length === 0) {
-          req.session.error = '정품 인증 페이지가 존재하지 않습니다. 먼저 페이지를 생성해주세요!';
-          res.redirect('/')
-        } else {
-          var pagedata = JSON.parse(rows[0]['pagedata'])
-          res.render('manage/edit', { data: data, rows: rows, pagedata: pagedata, help: help, options: options })
-        }
-      });
-
-    } else if(req.params.service == 3) {
+      var rows = await sqlp(sql, SqlString.format("SELECT * FROM pages WHERE service=2 AND owner=?", [req.user.id]))
+      if (rows.length == 0) {
+        req.session.error = '정품 인증 페이지가 존재하지 않습니다. 먼저 페이지를 생성해주세요!';
+        return res.redirect('/manage')
+      }
+      res.render('manage/edit', { data: data, rows: rows, pagedata: JSON.parse(rows[0]['pagedata']), help: help, options: options })
+    } else if (req.params.service == 3) {
       var data = {
         "name": "서버 상태 위젯",
         "service": 3
@@ -105,16 +97,12 @@ module.exports = (req, res) => {
         },
         "savetojson": ["sv_ip", "sv_port"]
       }
-      sql.query('select * from pages where service=3 and owner=' + SqlString.escape(req.user.id), function(err, rows){
-        if (rows.length === 0) {
-          req.session.error = '서버 상태 위젯이 존재하지 않습니다. 먼저 페이지를 생성해주세요!';
-          res.redirect('/manage')
-        } else {
-          var pagedata = JSON.parse(rows[0]['pagedata'])
-          res.render('manage/edit', { data: data, rows: rows, pagedata: pagedata, help: help, options: options })
-        }
-      });
-
+      var rows = await sqlp(sql, SqlString.format("SELECT * FROM pages WHERE service=3 AND owner=?", [req.user.id]))
+      if (rows.length == 0) {
+        req.session.error = '서버 상태 위젯이 존재하지 않습니다. 먼저 페이지를 생성해주세요!';
+        return res.redirect('/manage')
+      }
+      res.render('manage/edit', { data: data, rows: rows, pagedata: JSON.parse(rows[0]['pagedata']), options: options })
     } else {
       res.render('error/403')
     }
