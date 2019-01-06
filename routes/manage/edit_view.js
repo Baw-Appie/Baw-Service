4f6bc8cadc991dbb4fe6a0c7b0eea55c38a2ab61
Baw_Appie <pp121324@gmail.com>
2019-01-06 15:50:39 +0900

    도움말 변경

--- routes/manage/edit_view.js
+++ routes/manage/edit_view.js
@@ -37,14 +37,7 @@ module.exports = (req, res) => {
         },
         "savetojson": ["mail_ok", "sms_ok", "kakao_ok", "tg_ok", "browser_ok", "lookup_ok", "bouns","api_cmd","youtube", "disabled", "background"]
       }
-      var help = `<p>후원 보너스 설정의 다음줄은 || 으로 구분합니다.</p>
-		<p>후원 보너스 설정 예제: 캐시||칭호||Baw Service 최고</p>
-		<p>API 플러그인의 사용을 원치 않는경우 API 플러그인 명령어칸을 빈칸으로 설정해주세요.</p>
-		<p>API 플러그인 명령어 플레이스 홀더 [유저이름: &lt;player&gt;] [후원 금액: &lt;money&gt;] [후원 보상: &lt;package&gt;]</p>
-		<p>API 플러그인 명령어는 콘솔에서 실행됩니다. '/'를 입력할 필요가 없습니다.</p>
-		<p>API 플러그인 명령어 예제: say &lt;player&gt;님이 &lt;money&gt;원으로 &lt;package&gt;(을)를 구매했습니다!</p>
-		<p><a href='https://`+ req.hostname +`/api/API/edit'>[API 설정]</a></p>
-		<p>공지 사항란에 HTML을 사용할 수 있습니다.</p>`
+      var help = "openlink:https://baw-service.tistory.com/40"
 
       sql.query('select * from pages where service=1 and owner=' + SqlString.escape(req.user.id), function(err, rows){
         if (rows.length === 0) {
@@ -81,12 +74,7 @@ module.exports = (req, res) => {
         },
         "savetojson": ["mail_ok", "auto_process", "api_cmd"]
       }
-      var help = `<p>API 플러그인의 사용을 원치 않는경우 API 플러그인 명령어칸을 빈칸으로 설정해주세요.</p>
-		<p>API 플러그인 명령어 플레이스 홀더 [유저이름: &lt;player&gt;]</p>
-		<p>API 플러그인 명령어는 콘솔에서 실행됩니다. '/'를 입력할 필요가 없습니다.</p>
-		<p>API 플러그인 명령어 예제: say &lt;player&gt; 님이 Baw Service에서 정품인증을 완료했습니다!</p>
-		<p><a href='https://`+ req.hostname +`/api/API/edit'>[API 설정]</a></p>
-		<p>공지 사항란에 HTML을 사용할 수 있습니다.</p>`
+      var help = "openlink:https://baw-service.tistory.com/41"
 
       sql.query('select * from pages where service=2 and owner=' + SqlString.escape(req.user.id), function(err, rows){
         if (rows.length === 0) {
@@ -117,8 +105,6 @@ module.exports = (req, res) => {
         },
         "savetojson": ["sv_ip", "sv_port"]
       }
-      var help = `<p>공지 사항란에 HTML을 사용할 수 있습니다.</p>`
-
       sql.query('select * from pages where service=3 and owner=' + SqlString.escape(req.user.id), function(err, rows){
         if (rows.length === 0) {
           req.session.error = '서버 상태 위젯이 존재하지 않습니다. 먼저 페이지를 생성해주세요!';
