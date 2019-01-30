6ad25e692c7c5eeb76af64ce1fc5daec9de723f4
Baw_Appie <pp121324@gmail.com>
2019-01-30 13:15:19 +0900

    후원 차트 개선

--- libs/getdata.js
+++ libs/getdata.js
@@ -24,21 +24,22 @@ module.exports = async (req) => {
     var a1_data = countdata(await sqlp(sql, SqlString.format('SELECT * from service WHERE service=1 AND owner=? AND status=1', [req.user.id])), 'bal', /,/gi, true)
     var a2_data = countdata(await sqlp(sql, SqlString.format('SELECT * from service WHERE service=1 AND owner=? AND status=1 AND date > DATE_ADD(now(), INTERVAL -1 month)', [req.user.id])), 'bal', /,/gi, true)
     var a3_data = (await sqlp(sql, SqlString.format('SELECT * from service WHERE service=1 AND owner=? AND status=2', [req.user.id]))).length
-    var c1_data = countdata(await sqlp(sql, SqlString.format('SELECT * from service WHERE service=1 AND owner=? AND status=1 AND `date` BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL DAYOFMONTH(CURRENT_DATE)-1 DAY)  AND LAST_DAY(CURDATE());', [req.user.id])), 'bal', /,/gi, true)
-    var c2_data = countdata(await sqlp(sql, SqlString.format('SELECT * from service WHERE service=1 AND owner=? AND status=1 AND `date` BETWEEN date(CURRENT_DATE - interval 1 MONTH) - interval day(now()) day + interval 1 day AND LAST_DAY(CURRENT_DATE - interval 1 MONTH);', [req.user.id])), 'bal', /,/gi, true)
-    var c3_data = countdata(await sqlp(sql, SqlString.format('SELECT * from service WHERE service=1 AND owner=? AND status=1 AND `date` BETWEEN date(CURRENT_DATE - interval 2 MONTH) - interval day(now()) day + interval 1 day AND LAST_DAY(CURRENT_DATE - interval 2 MONTH);', [req.user.id])), 'bal', /,/gi, true)
+    var c1_req = await sqlp(sql, SqlString.format('SELECT date, extradata from service WHERE service=1 AND owner=? AND status=1 AND `date` BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY) AND CURDATE();', [req.user.id]))
+    var c1_data = []
+    await c1_req.forEachAsync(async (value) => {
+      c1_data.push({date: value.date, extradata: JSON.parse(value.extradata)})
+    })
+    c1_data = JSON.stringify(c1_data)
   } else {
     var a1_data = 0
     var a2_data = 0
     var a3_data = 0
     var c1_data = 0
-    var c2_data = 0
-    var c3_data = 0
   }
   if((await sqlp(sql, SqlString.format('SELECT * from pages WHERE service=2 AND owner=?', [req.user.id]))).length == 1){
     var a4_data = (await sqlp(sql, SqlString.format('SELECT * from service WHERE service=2 AND owner=? AND status=1', [req.user.id]))).length
   } else {
     var a4_data = 0
   }
-  return {a1: a1_data, a2: a2_data, a3: a3_data, a4: a4_data, c1: c1_data, c2: c2_data, c3: c3_data}
+  return {a1: a1_data, a2: a2_data, a3: a3_data, a4: a4_data, c1: c1_data}
 }
