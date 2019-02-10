a7ebd99a27203d6f5750c1dc5b2936c103ea21ba
Baw Appie <pp121324@gmail.com>
2019-02-10 21:41:04 +0900

    버그 수정

--- routes/api/getlist.js
+++ routes/api/getlist.js
@@ -6,8 +6,8 @@ module.exports = async (req, res) => {
   if(req.body.api_key && req.body.id){
     var { api_key, id } = req.body
     try {
-      var donation = await sqlp(sql, SqlString.format("SELECT * FROM api1 WHERE ?", { owner: id, api_key: api_key }))
-      var id_check = await sqlp(sql, SqlString.format("SELECT * FROM api2 WHERE ?", { owner: id, api_key: api_key }))
+      var donation = await sqlp(sql, SqlString.format("SELECT * FROM api1 WHERE ?", [{ owner: id, api_key: api_key }]))
+      var id_check = await sqlp(sql, SqlString.format("SELECT * FROM api2 WHERE ?", [{ owner: id, api_key: api_key }]))
       await sqlp(sql, SqlString.format('delete from api1 WHERE owner=? and api_key=?', [id, api_key]))
       await sqlp(sql, SqlString.format('delete from api2 WHERE owner=? and api_key=?', [id, api_key]))
     } catch(e) {
